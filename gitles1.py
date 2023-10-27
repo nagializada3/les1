7a67b6283d68ea66568b15654e8f3e56b8c6bb4a
nagializada3 <nagi.alizade8a2.2014@gmail.com>
2023-10-27 22:43:58 +0400

    no:20 exam result=60%

---
+++ gitles1.py
@@ -0,0 +1,15 @@
+n = int(input())
+m = n
+cnt = 0
+if len(str(n)) == 1:
+    print(1)
+else:
+    while n > 0:
+        s = n % 10
+        m-=s
+        n //= 10
+        if n == 0:
+            n = m
+            cnt+=1
+print(cnt)
+    
